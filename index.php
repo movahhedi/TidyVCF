567c47692e9f509e4b01e6fae58a2c81262325cd
Shahabeddin Movahhedi <s@shmovahhedi.com>
2021-08-12 00:16:29 +0430

    Sample VCF files

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 		else array_push($temp, $t);
 
 	}
-	
+
 
 	foreach ($contacts as $key => &$each) {
 		$hasTel = false;
@@ -30,9 +30,9 @@
 		if ( ! $hasTel) unset($contacts[$key]);
 		else {
 			if (false !== $key = array_search($each[0], $contacts2Names)) {
-                for ($i=1; $i < count($each); $i++) {
-                    if ( ! in_array($each[$i], $contacts2[$key])) array_push($contacts2[$key], $each[$i]);
-                }
+				for ($i=1; $i < count($each); $i++) {
+					if ( ! in_array($each[$i], $contacts2[$key])) array_push($contacts2[$key], $each[$i]);
+				}
 			} else {
 				array_push($contacts2, $each);
 				array_push($contacts2Names, $each[0]);
@@ -40,39 +40,43 @@
 		}
 
 	}
-	
-	
+
+
 	foreach ($contacts2 as $key => &$each) {
 		foreach ($each as $keyt => &$t) {
-            $foundcount = 0;
-            foreach ($each as $keyi => &$i) {
-                if (substr($i, -10) == substr($t, -10)) $foundcount++;
-            }
-            if ($foundcount >= 2) {
-                unset($each[$keyt]);
-                $foundcount = 0;
-            }
-            
-            $foundcountFN = 0;
-            foreach ($each as &$i) {
-                if (substr($i, 0, 3) === "FN:" &&substr($t, 0, 3) === "FN:" ) $foundcountFN++;
-            }
-            if ($foundcountFN >= 2) {
-                unset($each[$keyi]);
-                $foundcountFN = 0;
-            }
-        }
+			$foundcount = 0;
+			foreach ($each as $keyi => &$i) {
+				if (substr($i, -10) == substr($t, -10)) $foundcount++;
+			}
+			if ($foundcount >= 2) {
+				unset($each[$keyi]);
+				$foundcount = 0;
+			}
+		
+			$foundcountFN = 0;
+			foreach ($each as $keyi => &$i) {
+				if (substr($i, 0, 3) === "FN:" &&substr($t, 0, 3) === "FN:" ) $foundcountFN++;
+			}
+			if ($foundcountFN >= 2) {
+				unset($each[$keyi]);
+				$foundcountFN = 0;
+			}
+		}
 	}
 	var_dump($contacts2);
 
-    $s="";
-    foreach ($contacts2 as $key => $each) {
-        $s .= "BEGIN:VCARD\r\nVERSION:2.1\r\n";
-        foreach ($each as $keyt => $t) {
-            $s .= $t . "\r\n";
-        }
-        $s .= "END:VCARD\r\n";
-    }
+	//$contacts3 = array_merge($contacts2,);
+	/*$contacts3 = call_user_func_array('array_merge', $contacts2);
+	var_dump($contacts3);*/
 
-    file_put_contents("output.vcf", $s);
+	$s="";
+	foreach ($contacts2 as $each2) {
+		$s .= "BEGIN:VCARD\r\nVERSION:2.1\r\n";
+		foreach ($each2 as $t2) {
+			$s .= $t2 . "\r\n";
+		}
+		$s .= "END:VCARD\r\n";
+	}
+
+	file_put_contents("output.vcf", $s);
 ?>
